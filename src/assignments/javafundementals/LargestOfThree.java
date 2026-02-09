feb1202543343420f9262a5246ee478184fca1cf
ashokrajudh <ashokvarma.d789@gmail.com>
2026-02-09 16:06:52 +0530

    switch & if-else programs

---
+++ src/assignments/javafundementals/LargestOfThree.java
@@ -0,0 +1,19 @@
+package assignments.javafundementals;
+
+public class LargestOfThree {
+
+    public static void main(String[] args) {
+
+        int a = 25;
+        int b = 40;
+        int c = 30;
+
+        if (a >= b && a >= c) {
+            System.out.println("Largest number is: " + a);
+        } else if (b >= a && b >= c) {
+            System.out.println("Largest number is: " + b);
+        } else {
+            System.out.println("Largest number is: " + c);
+        }
+    }
+}
